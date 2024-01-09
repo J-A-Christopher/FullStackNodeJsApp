71f9c126e8702b99755f23a5698a7dd7fa1903de
J-A-Christopher <christopherjesse45@gmail.com>
2024-01-09 13:36:06 +0300

    Implememnting now using mongoose and not native mongodb commands

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const mongoConnect = require('./util/database').mongoConnect;
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const errorController = require('./controllers/error');
+const mongoose = require('mongoose');
 
-const User = require('./models/users');
+//const User = require('./models/users');
 
 
 
@@ -20,23 +21,24 @@ app.set('views','views');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next)=>{
-  User.findById('659c5bbf86a8e66878f87f36').then(user => {
-    //Here
-    req.user = new User(user.name, user.email, user.cart, user._id)
+// app.use((req, res, next)=>{
+//   User.findById('659c5bbf86a8e66878f87f36').then(user => {
+//     //Here
+//     req.user = new User(user.name, user.email, user.cart, user._id)
     
-    next();
-  }).catch(err=>console.log(err));
+//     next();
+//   }).catch(err=>console.log(err));
 
 
-});
+// });
 
 app.use('/admin',adminRoutes);
  app.use(shopRoutes);
 
 app.use(errorController.get404); 
-mongoConnect(() => {
-  
-  app.listen(3000)
-  
-}); 
+
+mongoose.connect('mongodb+srv://christopher:cjambetsa@cluster0.gxedhff.mongodb.net/shop?retryWrites=true&w=majority').then(result => {
+  app.listen(3000);
+}).catch(err => {
+  console.log(err);
+})
--- controllers/shop.js
+++ controllers/shop.js
@@ -59,20 +59,10 @@ exports.getCart =(req, res, next) =>{
 
 exports.postCartDeleteProduct = (req, res, next) =>{
     const prodId = req.body.productId;
-    req.user.getCart().then(cart=>{
-        return cart.getProducts({where:{id:prodId}}).then(products=>{
-            const product = products[0];
-            return product.cartItem.destroy();
-
-        }).then(result =>{
+    req.user.deleteItemFromCart(prodId).then(result =>{
             res.redirect('/cart');
 
         })
-    }).catch(err=>console.log(err));
-    // Product.findById(prodId, product =>{
-    //     Cart.deleteProduct(prodId, product.price);
-       
-    // });
 
 
 }
@@ -89,7 +79,7 @@ exports.postCart = (req, res, next) =>{
 }
 
 exports.getOrders =(req, res, next) =>{
-    req.user.getOrders({include: ['products']}).then(orders=>{
+    req.user.getOrders().then(orders=>{
         res.render('shop/orders', {
             pageTitle: 'Your Orders',
             path: '/order',
@@ -97,42 +87,14 @@ exports.getOrders =(req, res, next) =>{
           });
         
     }).catch(err=>console.log(err));
-    Product.findAll()
-//   .then(products => {
-//     res.render('shop/orders', {
-//       prods: products,
-//       pageTitle: 'Your Orders',
-//       path: '/order'
-//     });
-//   })
-  .catch(error => {
-    console.error('Error fetching products:', error);
-    res.status(500).send('Error fetching products');
-  }); 
+    
 };
 
 
 
 exports.postOrder = (req, res, next) =>{
     let fetchedCart;
-    req.user.getCart().then(cart=>{
-        fetchedCart = cart;
-        return cart.getProducts();
-    }).then(
-        products=>{
-           return req.user.createOrder().then(order =>{
-            return order.addProducts(products.map(product =>{
-                product.orderItem = {quantity: product.cartItem.quantity};
-                return product;
-            }));
-           }).catch(err=>console.log(err));
-        }
-    ).then(result=>{
-        return fetchedCart.setProducts(null);
-
-
-       
-    }).then(result =>{
+    req.user.addOrder().then(result =>{
         res.redirect('/orders');
 
     }).catch(err=>console.log(err))
